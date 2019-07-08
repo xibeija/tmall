fbaee76236172016df5e6a04e4ec2fbf352c9b36
xibeijia <xibeijia@xibeijia-PC>
2019-07-09 00:23:02 +0800

    fore

---
+++ src/tmall/servlet/ForeServlet.java
@@ -0,0 +1,21 @@
+package tmall.servlet;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import tmall.bean.Category;
+import tmall.dao.CategoryDAO;
+import tmall.dao.ProductDAO;
+import tmall.util.Page;
+
+public class ForeServlet extends BaseForeServlet {
+	public String home(HttpServletRequest request, HttpServletResponse response, Page page) {
+		List<Category> cs = new CategoryDAO().list();
+		new ProductDAO().fill(cs);
+		new ProductDAO().fillByRow(cs);
+		request.setAttribute("cs", cs);
+		return "home.jsp";
+	}
+}
